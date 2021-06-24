53f31a70fb900ee93b81275d828e8e8787b8c038
Vadim Timirkin <kva3umoda@gmail.com>
2021-06-24 12:55:36 +0500

    add templates

--- crossover/uniform.go
+++ crossover/uniform.go
@@ -6,17 +6,17 @@ import (
 )
 
 // Равномерное скрещивание
-type Uniform struct {
+type uniform struct {
 	indpd float64 // Independent probability for each attribute to be exchanged.
 }
 
-func NewUniform(indpd float64) *Uniform {
-	return &Uniform{
+func Uniform(indpd float64) Crossover {
+	return &uniform{
 		indpd: indpd,
 	}
 }
 
-func (tp *Uniform) Crossing(ind1, ind2 *genome.Individual) {
+func (tp *uniform) Crossing(ind1, ind2 *genome.Individual) {
 	size := min(len(ind1.Genome), len(ind2.Genome))
 	for i := 0; i < size; i++ {
 		if rand.Float() < tp.indpd {
---
+++ examples/05_restricted_tasks/03_graphs/README.md
@@ -0,0 +1,5 @@
+# Решение задачи о раскраске графа
+
+![Раскрска графа](https://upload.wikimedia.org/wikipedia/commons/9/91/Petersen1_tiny.svg)
+
+![Раскрска графа](https://upload.wikimedia.org/wikipedia/commons/9/90/Petersen_graph_3-coloring.svg)
